96b24203a59334b33d3c3040684e08aa98c7c8ce
OnurKaraduman <onury.karaduman@gmail.com>
2024-09-12 20:05:17 +0300

    Added Music List State

--- app/src/main/java/com/onurkaraduman/musifyapp/presentation/home/MusicListState.kt
+++ app/src/main/java/com/onurkaraduman/musifyapp/presentation/home/MusicListState.kt
@@ -1,23 +1,38 @@
-package com.onurkaraduman.musifyapp.presentation.home
-
-
+import com.onurkaraduman.musifyapp.common.Resource
 import com.onurkaraduman.musifyapp.domain.model.discover.DiscoverMusicModel
 import com.onurkaraduman.musifyapp.domain.model.genre.GenresModel
+import com.onurkaraduman.musifyapp.domain.model.popular.PopularMusicsModel
 
+sealed class MusicState {
+    data class Discover(val state: GenericMusicState<DiscoverMusicModel>) : MusicState()
+    data class Genres(val state: GenericMusicState<GenresModel>) : MusicState()
+    data class Popular(val state: GenericMusicState<PopularMusicsModel>) : MusicState()
+}
+
+data class GenericMusicState<T>(
+    val isLoading: Boolean = false,
+    val data: List<T> = emptyList(),
+    val error: String = ""
+)
 
 data class MusicListState(
-    val discoverMusicsState: DiscoverMusicsState = DiscoverMusicsState(),
-    val genresMusicsState: GenresMusicsState = GenresMusicsState(),
+    val discoverMusicState: MusicState.Discover = MusicState.Discover(GenericMusicState()),
+    val genresState: MusicState.Genres = MusicState.Genres(GenericMusicState()),
+    val popularMusicState: MusicState.Popular = MusicState.Popular(GenericMusicState())
 )
 
-data class DiscoverMusicsState(
-    val isLoading: Boolean = false,
-    val musics: List<DiscoverMusicModel> = emptyList(),
-    val error: String = ""
-)
+//
+//data class HomeScreenUIState(
+//    val isLoading: Boolean = false,
+//    val errorTypes: ErrorType? = null,
+//    val discoverMusicList: List<DiscoverMusicModel> = emptyList(),
+//    val genresMusicList: List<GenresModel> = emptyList(),
+//    val popularMusicList: List<PopularMusicsModel> = emptyList()
+//)
+//sealed class ErrorType{
+//   data class  ERROR_DISCOVER(val errorMessage:String):ErrorType()
+//   data class  ERROR_GENRES(val errorMessage:String):ErrorType()
+//   data class  ERROR_POPULAR(val errorMessage:String):ErrorType()
+//}
 
-data class GenresMusicsState(
-    val isLoading: Boolean = false,
-    val musics: List<GenresModel> = emptyList(),
-    val error: String = ""
-)
+
